d8de84b9ae448bd9ba2fb32fb7dce20f6d5f7c03
Lars Eckart <lars.eckart@hey.com>
2021-12-11 19:44:10 +0200

    add better documentation

--- start_point/HikerTest.java
+++ start_point/HikerTest.java
@@ -10,6 +10,11 @@ class HikerTest {
     @Test
     void testAnswer() throws Exception {
         int actual = Hiker.answer();
+        // if you want to change the expected result, 
+        // move HikerTest.testAnswer.received.txt
+        // to   HikerTest.testAnswer.approved.txt
+        // to view the differences, 
+        // open HikerTest.testAnswer.diff
         Approvals.verify("the answer is: " + actual);
     }
 }
