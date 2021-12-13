9a18815f0a463fd9b4dcffc7cbe97b78021e0a6f
Lars Eckart <lars.eckart@hey.com>
2021-12-13 08:31:37 +0200

    move comments above method

--- start_point/HikerTest.java
+++ start_point/HikerTest.java
@@ -7,14 +7,14 @@ import org.approvaltests.reporters.linux.ReportOnCyberDojo;
 @UseReporter(ReportOnCyberDojo.class)
 class HikerTest {
 
+    // if you want to change the expected result, 
+    // move HikerTest.testAnswer.received.txt
+    // to   HikerTest.testAnswer.approved.txt
+    // to view the differences, 
+    // open HikerTest.testAnswer.diff
     @Test
     void testAnswer() throws Exception {
         int actual = Hiker.answer();
-        // if you want to change the expected result, 
-        // move HikerTest.testAnswer.received.txt
-        // to   HikerTest.testAnswer.approved.txt
-        // to view the differences, 
-        // open HikerTest.testAnswer.diff
         Approvals.verify("the answer is: " + actual);
     }
 }
